7888d6b72cbe04cac1e1df2a413b79d08e7e6d9e
gabipr96 <gabi.rociana@gmail.com>
2024-06-07 14:14:21 +0200

    Mensaje cambiado

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def holamundo():
-    return render_template("index.html", msg="Hola Mundito jijiji!")
+    return render_template("index.html", msg="K pasó jaja Mundito jijiji!")
     
 
 if __name__ == '__main__':
